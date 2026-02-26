2ab674c73e9929d634d28f5ae7ca09ebe7e7b325
Nabeel vk <nabeelvk2006@gmail.com>
2026-02-26 13:41:33 +0530

    created JWT auth middle ware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -10,7 +10,7 @@ interface JwtPayload {
 export const authenticate = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     const authHeader = req.headers.authorization;
@@ -26,10 +26,12 @@ export const authenticate = async (
 
     const decoded = jwt.verify(
       token,
-      process.env.JWT_SECRET as string
+      process.env.JWT_SECRET as string,
     ) as JwtPayload;
 
-    const user = await User.findById(decoded.userId);
+    const user = await User.findById(decoded.userId).select(
+      "_id name email role verificationStatus",
+    );
 
     if (!user) {
       return res.status(401).json({
