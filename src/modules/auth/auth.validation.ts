996b9f76ae95299899f2bb834369864b985079d5
muhammed  arshaq tk <arshaqtk4@gmail.com>
2026-03-03 14:34:18 +0530

    feat: updated  password reset with OTP verification.

--- src/modules/auth/auth.validation.ts
+++ src/modules/auth/auth.validation.ts
@@ -72,12 +72,17 @@ export const resetPasswordSchema = z.object({
     .toLowerCase()
     .email('Invalid email address'),
 
-     password: z
-      .string({ error: 'Password is required' })
-      .min(8, 'Password must be at least 8 characters')
-      .regex(/[A-Z]/, 'Password must include at least one uppercase letter')
-      .regex(/[a-z]/, 'Password must include at least one lowercase letter')
-      .regex(/[0-9]/, 'Password must include at least one number'),
+  password: z
+    .string({ error: 'Password is required' })
+    .min(8, 'Password must be at least 8 characters')
+    .regex(/[A-Z]/, 'Password must include at least one uppercase letter')
+    .regex(/[a-z]/, 'Password must include at least one lowercase letter')
+    .regex(/[0-9]/, 'Password must include at least one number'),
+
+  otp: z
+    .string({ error: 'OTP is required' })
+    .trim()
+    .min(1, 'OTP is required'),
 });
 
 
