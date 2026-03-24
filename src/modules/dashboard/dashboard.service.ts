5e59e347395ee583a05402d7da2add4e9ba70a03
sharath <sharathsree916@gmail.com>
2026-03-24 15:53:07 +0530

    dashboard api chainged

--- src/modules/dashboard/dashboard.service.ts
+++ src/modules/dashboard/dashboard.service.ts
@@ -1,16 +1,41 @@
-export const getStudentDashboardData = async (userId:string) => {
+import { Enrollment } from "../enrollments/enrollment.model";
+import { User } from "../users/user.model";
+import { ApiError } from "../../utils/ApiError";
+import { getCourse, getCourses } from "../courses/course.service";
 
-   return {
-        enrolledCourses:[],
-        recommendedCourses:[],
-        creditBalance:0
-    }
-}
-export const getTutorDashboardData = async(userId:string) => {
+export const getStudentDashboardData = async (userId: string) => {
 
-    return {
-        createdCourses:[],
-        totalEnrollments:0,
-        earnings :0
-    }
-}
+   const enrollments = await Enrollment.find({ userId })
+    .populate({
+      path: "courseId",
+      populate: {
+        path: "tutorId",
+        select: "_id name avatarUrl"
+      }
+    });
+
+  const enrolledCourses = enrollments.map(e => e.courseId);
+const recommendedData = await getCourses(
+  {
+    page: 1,
+    limit: 4,
+    recommended: true,
+    minPrice: 0,
+    maxPrice: 100000
+  },
+  userId
+);
+
+  const recommendedCourses = recommendedData.courses;
+  const user = await User.findById(userId);
+
+  if (!user) throw new ApiError(404, "User not found");
+
+  const creditBalance = user.userCreditBalance;
+
+  return {
+    enrolledCourses,
+    recommendedCourses,
+    creditBalance
+  };
+};
