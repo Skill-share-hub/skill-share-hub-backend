ec881b6626d71f8dab5e0ce0b4eb3992a6a266c5
sharath <sharathsree916@gmail.com>
2026-03-24 18:45:07 +0530

    completed dash backend aip

--- src/modules/dashboard/dashboard.service.ts
+++ src/modules/dashboard/dashboard.service.ts
@@ -1,20 +1,56 @@
 import { Enrollment } from "../enrollments/enrollment.model";
 import { User } from "../users/user.model";
 import { ApiError } from "../../utils/ApiError";
-import { getCourse, getCourses } from "../courses/course.service";
+import {  getCourses } from "../courses/course.service";
 
 export const getStudentDashboardData = async (userId: string) => {
 
    const enrollments = await Enrollment.find({ userId })
-    .populate({
-      path: "courseId",
-      populate: {
+  .populate({
+    path: "courseId",
+    populate: [
+      {
         path: "tutorId",
         select: "_id name avatarUrl"
+      },
+      {
+        path: "contentModules", 
+        select: "duration"
       }
-    });
-
+    ]
+  });
+let totalHours = 0;
+let watchedHours = 0;
+let completedHours = 0;
   const enrolledCourses = enrollments.map(e => e.courseId);
+  const continueWatching = enrollments.filter(e => e.status === "active");
+  enrollments.forEach((enrollment: any) => {
+  const course = enrollment.courseId;
+
+  if (!course || !course.contentModules) return;
+
+  // total course duration
+  const courseTotal = course.contentModules.reduce(
+    (sum: number, content: any) => sum + (content.duration || 0),
+    0
+  );
+
+  totalHours += courseTotal;
+
+  // watched duration
+  const watched = course.contentModules
+    .filter((c: any) =>
+      enrollment.completedContent?.includes(c._id)
+    )
+    .reduce((sum: number, c: any) => sum + (c.duration || 0), 0);
+
+  watchedHours += watched;
+
+  // completed courses
+  if (enrollment.status === "completed") {
+    completedHours += courseTotal;
+  }
+});
 const recommendedData = await getCourses(
   {
     page: 1,
@@ -34,8 +70,12 @@ const recommendedData = await getCourses(
   const creditBalance = user.userCreditBalance;
 
   return {
-    enrolledCourses,
-    recommendedCourses,
-    creditBalance
-  };
+  enrolledCourses,
+  recommendedCourses,
+  creditBalance,
+  continueWatching,
+  totalHours,
+  watchedHours,
+  completedHours
+};
 };
